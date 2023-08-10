9ec2376fd6ebcc176768f653672c88992ab89419
Atalykis <55994420+Atalykis@users.noreply.github.com>
2023-08-10 19:09:08 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -61,7 +61,7 @@ wget -qO - https://regolith-desktop.org/regolith.key | \
 gpg --dearmor | sudo tee /usr/share/keyrings/regolith-archive-keyring.gpg > /dev/null
 
 echo deb "[arch=amd64 signed-by=/usr/share/keyrings/regolith-archive-keyring.gpg] \
-https://regolith-desktop.org/release-ubuntu-kinetic-amd64 kinetic main" | \
+https://regolith-desktop.org/release-ubuntu-jammy-amd64 jammy main" | \
 sudo tee /etc/apt/sources.list.d/regolith.list
 
 sudo apt update
